e7e20f71b7bf9e5b8afb528ea2307005863e7e81
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-07-07 08:39:44 -0300

    add link sociais e base login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,42 @@
+import { Social } from "../../components/social"
+import { FaFacebook, FaYoutube, FaInstagram, FaTiktok } from "react-icons/fa"
+
 export function Home(){
-    return(
-        <div>
-            <h1>page Home</h1>
-        </div>
-    )
+  return(
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Sujeito Programador</h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">
+              Canal no Youtube
+            </p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url='https://www.facebook.com/?locale=pt_BR'>
+           <FaFacebook size={35} color="#FFF"/>
+          </Social>
+
+          <Social url='https://www.instagram.com/'>
+           <FaInstagram size={35} color="#FFF"/>
+          </Social>
+
+          <Social url='https://www.youtube.com/'>
+           <FaYoutube size={35} color="#FFF"/>
+          </Social>
+
+          <Social url='https://www.tiktok.com/pt-BR/'>
+           <FaTiktok size={35} color="#FFF"/>
+          </Social>
+          
+        </footer>
+
+      </main>
+
+    </div>
+  )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router"
+
 export function Login(){
     return(
-    <div>
-        <h1>page Login</h1>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+        <Link to='/'>
+         <h1 className="mt-11 mb-7 font-bold text-5xl bg-gradient-to-t from-blue-800 to-blue-100 bg-clip-text text-transparent">Link
+            <span className="text-white">Maker</span>
+         </h1>
+
+        </Link>
+        
     </div>
     )
 }
